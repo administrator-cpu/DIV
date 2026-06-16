db374070d38e2f6d1f30c65649b45060fd6b6f4f
Harsh Jha <harsh.jha@grevive.in>
2026-06-16 11:24:14 +0530

    Add user/harsh local changes

--- components/Home/Components/Scroller.jsx
+++ components/Home/Components/Scroller.jsx
@@ -18,7 +18,7 @@ function Scroller() {
   };
 
   return (
-    <div className="w-full flex  px-4 relative z-20 ">
+    <div className="w-full flex  px-20 relative z-20  p-4 ">
       
       <div className="flex items-center gap-3 overflow-x-auto p-2 snap-x max-w-full [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']">
         {SectionNodes?.map((nav, idx) => (
@@ -26,7 +26,7 @@ function Scroller() {
             key={idx}
             href={`#${nav.toLowerCase().replace(/\s+/g, "-")}`}
             onClick={(e) => handleSmoothScroll(e, nav)}
-            className="shrink-0 whitespace-nowrap px-6 py-2.5 rounded-full  backdrop-blur-md font-bold text-[13px] tracking-wide text-gray-600 hover:text-[#561b5e] bg-[#fff]  hover:bg-[#fff] hover:shadow-sm transition-all duration-300 snap-center"
+            className="shrink-0 whitespace-nowrap px-6 py-2 rounded-full  backdrop-blur-md font-bold text-[14px]  tracking-wide text-gray-600 hover:text-[#561b5e] bg-[#fff]  hover:bg-[#fff] hover:shadow-sm transition-all duration-300 snap-center"
           >
             {nav}
           </a>
